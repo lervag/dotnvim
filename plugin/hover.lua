c19cf3357ffa2d6651b05d02149919b7678bd07d
Karl Yngve Lervåg <karl.yngve+git@gmail.com>
2022-01-12 23:25:15 +0100

    feat: define hover mapping → K

---
+++ plugin/hover.lua
@@ -0,0 +1,24 @@
+function MyHover()
+  if vim.tbl_contains({ 'vim', 'help' }, vim.bo.filetype) then
+    vim.cmd [[execute 'help ' . expand('<cword>')]]
+    return
+  end
+
+  if vim.bo.filetype == 'neomuttrc' then
+    local cword = vim.fn.expand('<cword>')
+    vim.cmd [[Man neomuttrc]]
+    vim.fn.search(cword)
+    return
+  end
+
+  if vim.bo.filetype == 'tex' then
+    vim.cmd 'VimtexDocPackage'
+    return
+  end
+
+  if not vim.tbl_isempty(vim.lsp.buf_get_clients()) then
+    vim.lsp.buf.hover()
+  end
+end
+
+vim.keymap.set('n', 'K', MyHover, { noremap=true, silent=true })
