5d9fe7e28fd6ab64901628298af28d12330da64a
Karl Yngve Lervåg <karl.yngve+git@gmail.com>
2022-03-25 15:07:37 +0100

    feat: improve cmp config

--- plugin/plugins/nvim-cmp.lua
+++ plugin/plugins/nvim-cmp.lua
@@ -1,4 +1,4 @@
-local cmp = require 'cmp'
+local cmp = require "cmp"
 
 local kind_icons = {
   Class = "ﴯ",
@@ -29,8 +29,8 @@ local kind_icons = {
 }
 
 local function formatter(entry, item)
-  if entry.source.name == 'omni' then
-    item.kind = 'Ѵ '
+  if entry.source.name == "omni" then
+    item.kind = "Ѵ"
     return item
   end
 
@@ -42,14 +42,15 @@ local function formatter(entry, item)
     ultisnips = "[snip]",
   })[entry.source.name]
   if not item.menu then
-    item.menu = string.format('[%s]', entry.source.name)
+    item.menu = string.format("[%s]", entry.source.name)
   end
 
   return item
 end
 
-local function t(str)
-  return vim.api.nvim_replace_termcodes(str, true, true, true)
+local function feedkeys(str)
+  vim.api.nvim_feedkeys(
+    vim.api.nvim_replace_termcodes(str, true, true, true), "m", true)
 end
 
 cmp.setup({
@@ -61,37 +62,50 @@ cmp.setup({
     keyword_length = 2,
   },
   sources = cmp.config.sources({
-    { name = 'nvim_lsp' },
-    { name = 'nvim_lua' },
-    { name = 'ultisnips' },
-    { name = 'path' },
-    { name = 'omni' },
-    { name = 'calc' },
-    { name = 'nvim_lsp_signature_help' },
-    { name = 'buffer', max_item_count = 5 },
+    { name = "nvim_lsp" },
+    { name = "nvim_lua" },
+    { name = "ultisnips" },
+    { name = "path" },
+    { name = "omni" },
+    { name = "calc" },
+    { name = "nvim_lsp_signature_help" },
   }),
   mapping = {
-    ['<c-space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
-    ['<c-b>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
-    ['<c-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
-    ['<c-u>'] = cmp.mapping.confirm({ select = true }),
-    ['<cr>'] = cmp.mapping.confirm({
+    ["<c-space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
+    ["<c-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-4), { "i", "c" }),
+    ["<c-f>"] = cmp.mapping(cmp.mapping.scroll_docs(4), { "i", "c" }),
+    ["<c-u>"] = cmp.mapping.confirm({ select = true }),
+    ["<c-j>"] = cmp.mapping(function(fallback)
+      if vim.fn["UltiSnips#CanJumpForwards"]() == 1 then
+        feedkeys("<plug>(ultisnips_jump_forward)")
+      else
+        fallback()
+      end
+    end),
+    ["<c-k>"] = cmp.mapping(function(fallback)
+      if vim.fn["UltiSnips#CanJumpBackwards"]() == 1 then
+        feedkeys("<plug>(ultisnips_jump_backward)")
+      else
+        fallback()
+      end
+    end),
+    ["<cr>"] = cmp.mapping.confirm({
       behavior = cmp.ConfirmBehavior.Replace,
-      select = true
+      select = false
     }),
     ["<tab>"] = cmp.mapping({
       i = function(fallback)
         if cmp.visible() then
           cmp.select_next_item({ behavior = cmp.SelectBehavior.Insert })
         elseif vim.fn["UltiSnips#CanJumpForwards"]() == 1 then
-          vim.api.nvim_feedkeys(t("<plug>(ultisnips_jump_forward)"), 'm', true)
+          feedkeys("<plug>(ultisnips_jump_forward)")
         else
           fallback()
         end
       end,
       s = function(fallback)
         if vim.fn["UltiSnips#CanJumpForwards"]() == 1 then
-          vim.api.nvim_feedkeys(t("<plug>(ultisnips_jump_forward)"), 'm', true)
+          feedkeys("<plug>(ultisnips_jump_forward)")
         else
           fallback()
         end
@@ -102,14 +116,14 @@ cmp.setup({
         if cmp.visible() then
           cmp.select_prev_item({ behavior = cmp.SelectBehavior.Insert })
         elseif vim.fn["UltiSnips#CanJumpBackwards"]() == 1 then
-          return vim.api.nvim_feedkeys( t("<plug>(ultisnips_jump_backward)"), 'm', true)
+          feedkeys("<plug>(ultisnips_jump_backward)")
         else
           fallback()
         end
       end,
       s = function(fallback)
         if vim.fn["UltiSnips#CanJumpBackwards"]() == 1 then
-          return vim.api.nvim_feedkeys( t("<plug>(ultisnips_jump_backward)"), 'm', true)
+          feedkeys("<plug>(ultisnips_jump_backward)")
         else
           fallback()
         end
