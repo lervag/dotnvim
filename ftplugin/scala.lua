d3606032a049c68867b264a09eb312f48273136f
Karl Yngve Lervåg <karl.yngve+git@gmail.com>
2022-05-16 23:22:29 +0200

    chore(dap): put lang specific config in ftplugin

---
+++ ftplugin/scala.lua
@@ -0,0 +1,21 @@
+local dap = require "dap"
+
+dap.configurations.scala = {
+  {
+    type = "scala",
+    request = "launch",
+    name = "RunOrTest",
+    metals = {
+      runType = "runOrTestFile",
+      --args = { "firstArg", "secondArg" },
+    },
+  },
+  {
+    type = "scala",
+    request = "launch",
+    name = "Test Target",
+    metals = {
+      runType = "testTarget",
+    },
+  },
+}
