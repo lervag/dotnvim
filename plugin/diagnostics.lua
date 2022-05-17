f2b12e4c2059c167089a2d69c63b23ce395c936e
Karl Yngve Lervåg <karl.yngve+git@gmail.com>
2022-05-17 23:40:44 +0200

    feat: add diagnostic toggle

--- plugin/diagnostics.lua
+++ plugin/diagnostics.lua
@@ -21,8 +21,19 @@ vim.fn.sign_define({
   { text = '', name = 'DiagnosticSignHint', texthl = 'DiagnosticSignHint' }
 })
 
+local diagnostics_active = true
+local toggle_diagnostics = function()
+  diagnostics_active = not diagnostics_active
+  if diagnostics_active then
+    vim.diagnostic.show()
+  else
+    vim.diagnostic.hide()
+  end
+end
+
+vim.keymap.set('n', '<leader>qq', toggle_diagnostics)
 vim.keymap.set('n', '<leader>qp', vim.diagnostic.goto_prev)
 vim.keymap.set('n', '<leader>qn', vim.diagnostic.goto_next)
-vim.keymap.set('n', '<leader>ql', vim.diagnostic.setloclist)
-vim.keymap.set('n', '<leader>qq', vim.diagnostic.setqflist)
+vim.keymap.set('n', '<leader>qL', vim.diagnostic.setloclist)
+vim.keymap.set('n', '<leader>ql', vim.diagnostic.setqflist)
 vim.keymap.set('n', '<leader>qe', vim.diagnostic.open_float)
