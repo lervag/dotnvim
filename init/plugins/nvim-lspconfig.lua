46f5c3139f9bb7d632a0f5a0cd0874c5e2df8887
Karl Yngve Lervåg <karl.yngve+git@gmail.com>
2022-05-18 14:38:59 +0200

    chore: refactor lsp configs

--- init/plugins/nvim-lspconfig.lua
+++ init/plugins/nvim-lspconfig.lua
@@ -7,90 +7,22 @@ capabilities = vim.tbl_extend('keep', capabilities, lsp_status.capabilities)
 capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
 
 
--- Lua
--- https://github.com/sumneko/lua-language-server
--- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#sumneko_lua
-local runtime_path = vim.split(package.path, ';')
-table.insert(runtime_path, 'lua/?.lua')
-table.insert(runtime_path, 'lua/?/init.lua')
+-- Use separate files for each desired LSP placed in
+-- ~/.config/nvim/lua/init/lsp/
+local servers = vim.tbl_map(
+  function(x)
+    return x:match("([^/]*).lua$")
+  end,
+  vim.api.nvim_get_runtime_file("lua/init/lsp/*.lua", true)
+)
 
-lc.sumneko_lua.setup {
-  capabilities = capabilities,
-  settings = {
-    Lua = {
-      runtime = {
-        version = 'LuaJIT',
-        path = runtime_path,
-      },
-      workspace = {
-        library = vim.api.nvim_get_runtime_file('', true),
-      },
-      diagnostics = {
-        globals = { 'vim' },
-        disable = { 'lowercase-global' },
-      },
-      -- completion = {
-      --   callSnippet = 'Replace',
-      --   showWord = 'Disable',
-      -- },
-      telemetry = {
-        enable = false,
-      },
-    },
-  },
-}
-
--- Python
--- https://github.com/microsoft/pyright
--- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#pyright
-lc.pyright.setup {
-  capabilities = capabilities,
-}
-
--- Kotlin
--- https://github.com/fwcd/kotlin-language-server
--- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#kotlin_language_server
-lc.kotlin_language_server.setup {
-  capabilities = capabilities,
-  handlers = {
-    ["textDocument/publishDiagnostics"] = function() end
-  }
-}
-
--- Java
--- https://github.com/georgewfraser/java-language-server
--- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#java_language_server
-lc.java_language_server.setup {
-  cmd = { "java-language-server" },
-  capabilities = capabilities,
-  handlers = {
-    ["textDocument/publishDiagnostics"] = function() end
-  }
-}
-
-
--- Other
-lc.vimls.setup {
-  capabilities = capabilities,
-}
-lc.bashls.setup {
-  capabilities = capabilities,
-}
-lc.jsonls.setup {
-  cmd = { "vscode-json-languageserver", "--stdio" },
-  capabilities = capabilities,
-}
-lc.yamlls.setup {
-  capabilities = capabilities,
-}
-lc.rust_analyzer.setup {
-  capabilities = capabilities,
-}
-lc.cssls.setup {
-  capabilities = capabilities,
-}
-lc.html.setup {
-  capabilities = capabilities,
-}
--- lc.ltex.setup {}
--- lc.texlab.setup {}
+for _, server in pairs(servers) do
+  local _, user_opts = pcall(require, "init.lsp." .. server)
+  local opts = vim.tbl_deep_extend('force', {
+    capabilities = capabilities,
+    flags = {
+      debounce_text_changes = 150
+    }
+  }, user_opts)
+  lc[server].setup(opts)
+end
---
+++ lua/init/lsp/sumneko_lua.lua
@@ -0,0 +1,31 @@
+-- Configuration for Sumneko Lua LSP
+-- https://github.com/sumneko/lua-language-server
+-- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#sumneko_lua
+local runtime_path = vim.split(package.path, ';')
+table.insert(runtime_path, 'lua/?.lua')
+table.insert(runtime_path, 'lua/?/init.lua')
+
+return {
+  settings = {
+    Lua = {
+      runtime = {
+        version = 'LuaJIT',
+        path = runtime_path,
+      },
+      workspace = {
+        library = vim.api.nvim_get_runtime_file('', true),
+      },
+      diagnostics = {
+        globals = { 'vim' },
+        disable = { 'lowercase-global' },
+      },
+      -- completion = {
+      --   callSnippet = 'Replace',
+      --   showWord = 'Disable',
+      -- },
+      telemetry = {
+        enable = false,
+      },
+    },
+  },
+}
