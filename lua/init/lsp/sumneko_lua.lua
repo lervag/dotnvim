5e7adc7356d4699cdf1f4b1ebe492bc5e2c8d7a9
Karl Yngve Lervåg <karl.yngve+git@gmail.com>
2022-05-23 14:19:16 +0200

    feat: update yamlls and luals settings

--- lua/init/lsp/sumneko_lua.lua
+++ lua/init/lsp/sumneko_lua.lua
@@ -1,6 +1,7 @@
 -- Configuration for Sumneko Lua LSP
 -- https://github.com/sumneko/lua-language-server
 -- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#sumneko_lua
+-- https://github.com/sumneko/lua-language-server/blob/master/locale/en-us/setting.lua
 local runtime_path = vim.split(package.path, ';')
 table.insert(runtime_path, 'lua/?.lua')
 table.insert(runtime_path, 'lua/?/init.lua')
@@ -19,10 +20,10 @@ return {
         globals = { 'vim' },
         disable = { 'lowercase-global' },
       },
-      -- completion = {
-      --   callSnippet = 'Replace',
-      --   showWord = 'Disable',
-      -- },
+      completion = {
+        callSnippet = 'Replace',
+        keywordSnippet = 'Replace',
+      },
       telemetry = {
         enable = false,
       },
