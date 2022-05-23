e5257384a43f0c19c30e265d2789ff8e0d578de0
Karl Yngve Lervåg <karl.yngve+git@gmail.com>
2022-05-23 14:35:25 +0200

    chore: adjust sumneko settings

--- lua/init/lsp/sumneko_lua.lua
+++ lua/init/lsp/sumneko_lua.lua
@@ -1,17 +1,12 @@
 -- Configuration for Sumneko Lua LSP
 -- https://github.com/sumneko/lua-language-server
--- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#sumneko_lua
 -- https://github.com/sumneko/lua-language-server/blob/master/locale/en-us/setting.lua
-local runtime_path = vim.split(package.path, ';')
-table.insert(runtime_path, 'lua/?.lua')
-table.insert(runtime_path, 'lua/?/init.lua')
-
+-- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#sumneko_lua
 return {
   settings = {
     Lua = {
       runtime = {
         version = 'LuaJIT',
-        path = runtime_path,
       },
       workspace = {
         library = vim.api.nvim_get_runtime_file('', true),
