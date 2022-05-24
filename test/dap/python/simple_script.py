7e55fae1bba8f874bf66cdb5f18ecc36027f7ab9
Karl Yngve Lervåg <karl.yngve+git@gmail.com>
2022-05-24 23:04:50 +0200

    test: add dap test script for python

---
+++ test/dap/python/simple_script.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+"""This is a sample script for testing dap functionality.
+
+Start debug session with ":Debugpy program".
+"""
+
+import sys
+from subprocess import call, run
+
+from docopt import docopt
+
+from pynotifier import Notification
+
+args = docopt(__doc__)
+
+try:
+    output = run('light', check=True, capture_output=True)
+    value = float(output.stdout.split(b"\n")[0])
+    if len(output.stderr) > 0:
+        print('N/A')
+        sys.exit()
+except FileNotFoundError:
+    print('light not available')
+    sys.exit()
+
+if value > 10:
+    r = -1
+    inc = 10
+else:
+    r = 0
+    inc = 1
+
+if args['up']:
+    if value > 9.5:
+        r = -1
+        inc = 10
+    value = min(round(value, r) + inc, 100)
+elif args['down']:
+    value = max(round(value, r) - inc, 1)
+
+call(f'light -S {value}', shell=True)
+
+value = "{:.0f} %".format(value)
+if args['--notify']:
+    Notification(
+        title='brightness-control',
+        description="Brightness: " + value,
+        icon_path='N/A',
+        duration=2,
+        urgency='normal'
+    ).send()
+else:
+    print(value)
