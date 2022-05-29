c260f8df9ce8ee6c2e41a76b748887898ca1016d
Karl Yngve Lervåg <karl.yngve+git@gmail.com>
2022-05-29 11:42:38 +0200

    feat: add copy mappings for gomove

--- init/plugins/gomove.lua
+++ init/plugins/gomove.lua
@@ -8,7 +8,17 @@ vim.keymap.set("n", "<down>",  "<plug>GoNSMDown")
 vim.keymap.set("n", "<up>",    "<plug>GoNSMUp")
 vim.keymap.set("n", "<right>", "<plug>GoNSMRight")
 
+vim.keymap.set("n", "<c-left>",  "<plug>GoNSDLeft")
+vim.keymap.set("n", "<c-down>",  "<plug>GoNSDDown")
+vim.keymap.set("n", "<c-up>",    "<plug>GoNSDUp")
+vim.keymap.set("n", "<c-right>", "<plug>GoNSDRight")
+
 vim.keymap.set("x", "<left>",  "<plug>GoVSMLeft")
 vim.keymap.set("x", "<down>",  "<plug>GoVSMDown")
 vim.keymap.set("x", "<up>",    "<plug>GoVSMUp")
 vim.keymap.set("x", "<right>", "<plug>GoVSMRight")
+
+vim.keymap.set("x", "<c-left>",  "<plug>GoVSDLeft")
+vim.keymap.set("x", "<c-down>",  "<plug>GoVSDDown")
+vim.keymap.set("x", "<c-up>",    "<plug>GoVSDUp")
+vim.keymap.set("x", "<c-right>", "<plug>GoVSDRight")
