d428b07c58bff22a8a15b6dce3ccf7b623737889
Karl Yngve Lervåg <karl.yngve+git@gmail.com>
2022-06-13 17:44:24 +0200

    feat: improve kotlin lsp settings

--- lua/init/lsp/kotlin_language_server.lua
+++ lua/init/lsp/kotlin_language_server.lua
@@ -1,8 +1,7 @@
 -- Kotlin Language Server
 -- https://github.com/fwcd/kotlin-language-server
 -- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#kotlin_language_server
+
 return {
-  handlers = {
-    ["textDocument/publishDiagnostics"] = function() end
-  }
+  root_dir = require('lspconfig.util').find_git_ancestor
 }
