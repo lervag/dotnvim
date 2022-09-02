8d979c8ac0a82a248f16c652196c8766bcea85e8
Karl Yngve Lervåg <karl.yngve+git@gmail.com>
2022-09-02 17:21:10 +0200

    feat: add initial ltex settings

---
+++ lua/init/lsp/ltex.lua
@@ -0,0 +1,27 @@
+-- https://valentjn.github.io/ltex/
+-- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#ltex
+
+-- local path = vim.fn.stdpath 'config' .. '/spell/en.utf-8.add'
+-- local words = {}
+
+-- for word in io.open(path, 'r'):lines() do
+--   table.insert(words, word)
+-- end
+
+return {
+  settings = {
+    ltex = {
+      -- enabled = { "latex", "markdown" },
+      checkFrequency="save",
+      language = "en-GB",
+      disabledRules = {
+        ["en-US"] = { "PROFANITY" },
+        ["en-GB"] = { "PROFANITY" },
+      },
+      -- dictionary = {
+      --   ["en-US"] = words,
+      --   ["en-GB"] = words,
+      -- },
+    },
+  },
+}
