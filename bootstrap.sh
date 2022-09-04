3c640193c1166a88641ad85b206f01396bf7b39d
Karl Yngve Lervåg <karl.yngve+git@gmail.com>
2022-09-04 17:45:24 +0200

    fix: regression in bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,23 +5,23 @@ rtp=$(dirname "$0")
 destination="${rtp}/autoload/plug.vim"
 
 
-# echo -n "Fetching vim-plug ... "
-# if [ -d "$target" ]; then
-#   echo "already fetched."
-# else
-#   echo "done!"
-#   mkdir -p "$(dirname "$target")"
-#   git clone --depth 1 https://github.com/junegunn/vim-plug.git "$target"
-# fi
+echo -n "Fetching vim-plug ... "
+if [ -d "$target" ]; then
+  echo "already fetched."
+else
+  echo "done!"
+  mkdir -p "$(dirname "$target")"
+  git clone --depth 1 https://github.com/junegunn/vim-plug.git "$target"
+fi
 
-# echo -n "Installing vim-plug ... "
-# if [ -e "$destination" ]; then
-#   echo "already installed."
-# else
-#   echo "done!"
-#   mkdir -p "$(dirname "$destination")"
-#   ln -s "$target/plug.vim" "$destination"
-# fi
+echo -n "Installing vim-plug ... "
+if [ -e "$destination" ]; then
+  echo "already installed."
+else
+  echo "done!"
+  mkdir -p "$(dirname "$destination")"
+  ln -s "$target/plug.vim" "$destination"
+fi
 
 spelldir="$HOME/documents/aux/vim/spell"
 echo -n "Preparing shared spell files ... "
