874a87e2315832529bdd99ba6e9de8ef41a88e36
Karl Yngve Lervåg <karl.yngve+git@gmail.com>
2022-11-27 13:24:57 +0100

    fix: avoid annoying popup

--- lua/init/lsp/sumneko_lua.lua
+++ lua/init/lsp/sumneko_lua.lua
@@ -10,6 +10,7 @@ return {
       },
       workspace = {
         library = vim.api.nvim_get_runtime_file('', true),
+        checkThirdParty = false,
       },
       diagnostics = {
         globals = { 'vim' },
