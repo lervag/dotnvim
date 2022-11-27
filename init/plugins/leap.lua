67c2531e9eca27cad2c46175b589fe776be1df03
Karl Yngve Lervåg <karl.yngve+git@gmail.com>
2022-11-27 13:24:43 +0100

    feat: update leap config

--- init/plugins/leap.lua
+++ init/plugins/leap.lua
@@ -1,13 +1,12 @@
-leap = require('leap').setup {
-  case_sensitive = true,
-  labels = {
+opts = require('leap').opts
+opts.case_sensitive = true
+opts.labels = {
     "s", "f", "n", "j", "k",
     "l", "o", "d", "w", "e",
     "h", "m", "v", "g", "u",
     "t", "c", ".", "z"
-  },
-  safe_labels = {},
-}
+  }
+opts.safe_labels = {}
 
 vim.keymap.set('n', 's', '<plug>(leap-forward)')
 vim.keymap.set('n', 'S', '<plug>(leap-backward)')
