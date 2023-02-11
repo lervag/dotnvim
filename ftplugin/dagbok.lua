3327951419930a0e5bd1344ff146748e53ca2133
Karl Yngve Lervåg <karl.yngve+git@gmail.com>
2023-02-11 13:30:30 +0100

    chore: more vimscript to lua

---
+++ ftplugin/dagbok.lua
@@ -0,0 +1,22 @@
+vim.o.hlsearch = false
+vim.opt_local.formatoptions:remove "n"
+vim.wo.signcolumn = "no"
+vim.wo.foldmethod = "expr"
+vim.wo.foldexpr = "getline(v:lnum) =~# '^\\d' ? '>1' : '1'"
+
+vim.api.nvim_create_autocmd("BufEnter", {
+  pattern = "dagbok.txt",
+  once = true,
+  command = [[silent! normal {,tlzv]]
+})
+
+for lhs, rhs in pairs({
+  [",t"] = [[/\C\%18c \?x<cr>]],
+  [",n"] = "Gonew<c-r>=UltiSnips#ExpandSnippet()<cr>",
+}) do
+  vim.keymap.set("n", lhs, rhs, { buffer = true })
+end
+vim.keymap.set("n", ",a", "zRgg/^2006-<cr>?^200<cr>k2yy}Pj$<c-a>oadd", {
+  buffer = true,
+  remap = true
+})
--- ftplugin/fugitive.vim
+++
@@ -1,2 +0,0 @@
-nnoremap <buffer><silent> q :bwipeout!<cr>
-nnoremap <buffer><silent> <f5> :call fugitive#ReloadStatus(-1, 0)<cr>
