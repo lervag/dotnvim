b4d87fd2c27cf63d3504706b35848ddf672b3a7c
Karl Yngve Lervåg <karl.yngve+git@gmail.com>
2023-02-15 21:49:43 +0100

    chore: combine dagbok indent and ftplugin

--- ftplugin/dagbok.lua
+++ ftplugin/dagbok.lua
@@ -1,8 +1,33 @@
-vim.o.hlsearch = false
-vim.opt_local.formatoptions:remove "n"
+function GetDagbokIndent(lnum_in)
+  if lnum_in == 1 then
+    return 0
+  end
+
+  -- Zero indent for first line of each entry
+  local cline = vim.fn.getline(lnum_in)
+  if cline:match "^%d+" then
+    return 0
+  end
+
+  -- Indent 18 for continuation of notes
+  local pline = vim.fn.getline(lnum_in - 1)
+  if pline:match "^%s+Notat" then
+    return 18
+  elseif pline:match "^%s+Snop" then
+    return 18
+  elseif pline:match "^%d+" then
+    return 2
+  end
+
+  return vim.fn.indent(lnum_in - 1)
+end
+
 vim.wo.signcolumn = "no"
 vim.wo.foldmethod = "expr"
 vim.wo.foldexpr = "getline(v:lnum) =~# '^\\d' ? '>1' : '1'"
+
+vim.opt_local.formatoptions:remove "n"
+vim.bo.indentexpr = "v:lua.GetDagbokIndent(v:lnum)"
 
 vim.api.nvim_create_autocmd("BufEnter", {
   pattern = "dagbok.txt",
