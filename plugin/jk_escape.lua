1ca3e33f948add3e512fd952af48eb6c16679638
Karl Yngve Lervåg <karl.yngve+git@gmail.com>
2023-02-15 22:35:23 +0100

    chore: jk_escape -> lua

---
+++ plugin/jk_escape.lua
@@ -0,0 +1,26 @@
+local reltime = vim.fn.reltime
+local reltimefloat = vim.fn.reltimefloat
+
+local function escape()
+  local timediff = reltimefloat(reltime(vim.w.__esc_time_j or {0, 0}))
+  vim.w.__esc_time_j = {0, 0}
+
+  if timediff <= 0.25 then
+    return "<bs><esc>"
+  end
+
+  return "k"
+end
+
+vim.keymap.set("i", "k", escape, {
+  desc = "jk escape",
+  expr = true
+})
+
+group = vim.api.nvim_create_augroup("init_escape", { clear = true })
+vim.api.nvim_create_autocmd("InsertCharPre", {
+  group = group,
+  callback = function()
+    vim.w.__esc_time_j = vim.v.char == 'j' and reltime() or {0, 0}
+  end
+})
