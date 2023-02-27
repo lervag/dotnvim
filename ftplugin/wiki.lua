8dfd643be91ef1a8ba076ed943f76876f03bd2d2
Karl Yngve Lervåg <karl.yngve+git@gmail.com>
2023-02-27 15:17:52 +0100

    fix: <c-e> for lists+wiki stopped working after lazy.nvim

--- ftplugin/wiki.lua
+++ ftplugin/wiki.lua
@@ -3,7 +3,7 @@ vim.fn["personal#markdown#init"]()
 vim.opt_local.isfname:remove ","
 
 vim.keymap.set("i", "<c-l>", "LLW", { buffer = true })
-vim.keymap.set("i", "<c-e>", "<plug>(ultisnips_jump_forward)<cr><c-a>LLW<c-r>=UltiSnips#ExpandSnippet()<cr>", { buffer = true, silent = true })
+vim.keymap.set("i", "<c-e>", "<plug>(cmpu-jump-forwards)<cr><plug>(lists-new-element)LLW<plug>(cmpu-expand)", { buffer = true, silent = true })
 
 vim.cmd.runtime "ftplugin/markdown/medieval.vim"
 vim.keymap.set("n", "<leader>ar", "<Plug>(medieval-eval)", { buffer = true })
