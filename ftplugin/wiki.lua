b91f61a186d2d98472131bc464e75bb9e16f6889
Karl Yngve Lervåg <karl.yngve+git@gmail.com>
2023-02-28 09:18:35 +0100

    fix: annoying thing about medieval

--- ftplugin/wiki.lua
+++ ftplugin/wiki.lua
@@ -5,5 +5,4 @@ vim.opt_local.isfname:remove ","
 vim.keymap.set("i", "<c-l>", "LLW", { buffer = true })
 vim.keymap.set("i", "<c-e>", "<plug>(cmpu-jump-forwards)<cr><plug>(lists-new-element)LLW<plug>(cmpu-expand)", { buffer = true, silent = true })
 
-vim.cmd.runtime "ftplugin/markdown/medieval.vim"
-vim.keymap.set("n", "<leader>ar", "<Plug>(medieval-eval)", { buffer = true })
+vim.keymap.set("n", "<leader>ar", "<cmd>call medieval#eval()<cr>", { buffer = true })
