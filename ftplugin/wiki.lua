427ba4c77cfbdc4bead4b1e73f9f254e7b182e91
Karl Yngve Lervåg <karl.yngve+git@gmail.com>
2023-03-09 11:12:06 +0100

    fix: mapping conflict lists-new-element

--- ftplugin/wiki.lua
+++ ftplugin/wiki.lua
@@ -3,6 +3,8 @@ vim.fn["personal#markdown#init"]()
 vim.opt_local.isfname:remove ","
 
 vim.keymap.set("i", "<c-l>", "LLW", { buffer = true })
-vim.keymap.set("i", "<c-e>", "<plug>(cmpu-jump-forwards)<cr><plug>(lists-new-element)LLW<plug>(cmpu-expand)", { buffer = true, silent = true })
+vim.keymap.set("i", "<c-e>",
+  "<plug>(cmpu-jump-forwards)<cr><c-a>LLW<plug>(cmpu-expand)",
+  { buffer = true, silent = true, remap = true })
 
 vim.keymap.set("n", "<leader>ar", "<cmd>call medieval#eval()<cr>", { buffer = true })
