eb74800d6cc1a06c2fc4578d51a9773951075ca7
Karl Yngve Lervåg <karl.yngve+git@gmail.com>
2023-06-24 18:42:27 +0200

    feat: ensure vimtex compile refreshes statusline

--- ftplugin/tex.lua
+++ ftplugin/tex.lua
@@ -3,3 +3,19 @@ vim.opt_local.wrap = true
 
 vim.keymap.set("n", "<cr>", "<plug>(vimtex-context-menu)", { buffer = true })
 vim.keymap.set("n", "K", "<plug>(vimtex-doc-package)", { buffer = true })
+
+-- Ensure statusline is refreshed
+vim.api.nvim_create_autocmd("User", {
+  pattern = {
+    "VimtexEventCompileStarted",
+    "VimtexEventCompileStopped",
+    "VimtexEventCompileSuccess",
+    "VimtexEventCompileFailed",
+    "VimtexEventCompiling",
+  },
+  desc = "Refresh statusline",
+  group = vim.api.nvim_create_augroup("init_vimtex", { clear = true }),
+  callback = function()
+    vim.cmd "redrawstatus"
+  end,
+})
