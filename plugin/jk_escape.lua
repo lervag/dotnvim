98ea90eaf16fbf51556819a71a28c85fa1028647
Karl Yngve Lervåg <karl.yngve+git@gmail.com>
2024-03-31 12:16:59 +0200

    chore: remove unnecessary lint disablers

--- plugin/jk_escape.lua
+++ plugin/jk_escape.lua
@@ -3,7 +3,6 @@ local reltimefloat = vim.fn.reltimefloat
 
 ---@return string
 local function escape()
-  ---@diagnostic disable-next-line
   local timediff = reltimefloat(reltime(vim.w.__esc_time_j or { 0, 0 }))
   vim.w.__esc_time_j = { 0, 0 }
 
@@ -23,7 +22,6 @@ local group = vim.api.nvim_create_augroup("init_escape", {})
 vim.api.nvim_create_autocmd("InsertCharPre", {
   group = group,
   callback = function()
-    ---@diagnostic disable-next-line
     vim.w.__esc_time_j = vim.v.char == "j" and reltime() or { 0, 0 }
   end,
 })
