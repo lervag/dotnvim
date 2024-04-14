287237d68dd318eda7cab49fe98a6696ea28666c
Karl Yngve Lervåg <karl.yngve+git@gmail.com>
2024-04-14 20:25:36 +0200

    feat: disable autoformat for lua

--- ftplugin/lua.lua
+++ ftplugin/lua.lua
@@ -1,17 +1,5 @@
 local dap = require "dap"
 
-vim.api.nvim_create_autocmd("BufWritePre", {
-  buffer = vim.api.nvim_get_current_buf(),
-  group = vim.api.nvim_create_augroup("init_lua", {}),
-  callback = function(arg)
-    if arg.file:sub(1, 10) == "fugitive:/" then
-      return
-    end
-
-    require("conform").format { bufnr = arg.buf }
-  end,
-})
-
 -- Adapters
 dap.adapters.nlua = function(callback, config)
   callback {
