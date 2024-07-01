5b774b5ffd153c3692e956c4c3a93584b39db90f
Karl Yngve Lervåg <karl.yngve+git@gmail.com>
2024-07-01 22:45:53 +0200

    feat: use oil.nvim for 'nvim .'

--- lua/lervag/init/packages.lua
+++ lua/lervag/init/packages.lua
@@ -381,6 +381,7 @@ local M = {
 
   {
     "stevearc/oil.nvim",
+    lazy = false,
     dependencies = { "nvim-tree/nvim-web-devicons" },
     keys = {
       { "-", "<cmd>Oil<cr>", desc = "Open parent directory" },
