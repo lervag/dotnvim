a06f622551c6360698ba8e838a461131bf4a79c8
Karl Yngve Lervåg <karl.yngve+git@gmail.com>
2024-08-11 18:28:36 +0200

    feat: use and adjust vim.ui.open

---
+++ plugin/open.lua
@@ -0,0 +1,14 @@
+local open = vim.ui.open
+
+---@param path string Path or URL to open
+---@return vim.SystemObj|nil # Command object, or nil if not found.
+---@return nil|string # Error message on failure, or nil on success.
+local function open_adjusted(path)
+  if path:match "^[%w-_]+/[%w-_]+$" then
+    path = "https://github.com/" .. path
+  end
+
+  return open(path)
+end
+
+vim.ui.open = open_adjusted
