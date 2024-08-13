4cff647d313dd5180cf3e3fbfaa48d323703e234
Karl Yngve Lervåg <karl.yngve+git@gmail.com>
2024-08-13 12:54:55 +0200

    fix: minor adjustment of open_adjusted

--- plugin/open.lua
+++ plugin/open.lua
@@ -4,7 +4,7 @@ local open = vim.ui.open
 ---@return vim.SystemObj|nil # Command object, or nil if not found.
 ---@return nil|string # Error message on failure, or nil on success.
 local function open_adjusted(path)
-  if path:match "^[%w-_]+/[%w-_]+$" then
+  if path:match "^[%w-_.]+/[%w-_.]+$" then
     path = "https://github.com/" .. path
   end
 
