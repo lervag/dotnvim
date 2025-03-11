1e4de5b7db42291c8521ea7f72d30f6167149cf4
Karl Yngve Lervåg <karl.yngve+git@gmail.com>
2025-03-11 23:40:38 +0100

    feat: use marker folding for vim files if applicable

--- ftplugin/vim.lua
+++ ftplugin/vim.lua
@@ -1,6 +1,10 @@
--- vim.wo.foldmethod = "marker"
-vim.wo.foldmethod = "expr"
-vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+if vim.fn.search([[{{{\d\?$]], "nw") > 0
+then
+  vim.wo.foldmethod = "marker"
+else
+  vim.wo.foldmethod = "expr"
+  vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+end
 
 vim.g.ruby_fold = nil
 vim.g.vimsyn_folding = "f"
