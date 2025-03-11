9919c5d7f8e35eb313f15637332a31376aa6498d
Karl Yngve Lervåg <karl.yngve+git@gmail.com>
2025-03-11 23:40:54 +0100

    feat: update gp.nvim models

--- lua/lervag/init/packages.lua
+++ lua/lervag/init/packages.lua
@@ -468,20 +468,20 @@ local M = {
             .. "- Take a deep breath; You've got this!\n"
         },
         {
+          name = "o3-mini",
+          chat = true,
+          command = false,
+          model = { model = "o3-mini", temperature = 1.1, top_p = 1 },
+          system_prompt = "You are a general AI assistant.\n"
+            .. "You will respond consisely and as brief as you can.\n"
+        },
+        {
           name = "o1-mini",
           chat = true,
           command = false,
           model = { model = "o1-mini", temperature = 1.1, top_p = 1 },
-          system_prompt = "You are a general AI assistant.\n\n"
-            .. "The user provided the additional info about how they would like you to respond:\n\n"
-            .. "- Be brief and consise!\n"
-            .. "- If you're unsure don't guess and say you don't know instead.\n"
-            .. "- Ask question if you need clarification to provide better answer.\n"
-            .. "- Think deeply and carefully from first principles step by step.\n"
-            .. "- Zoom out first to see the big picture and then zoom in to details.\n"
-            .. "- Use Socratic method to improve your thinking and coding skills.\n"
-            .. "- Don't elide any code from your output if the answer requires coding.\n"
-            .. "- Take a deep breath; You've got this!\n"
+          system_prompt = "You are a general AI assistant.\n"
+            .. "You will respond consisely and as brief as you can.\n"
         },
       },
     },
