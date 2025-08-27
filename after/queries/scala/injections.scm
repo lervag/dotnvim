ae4e8c2b00786296be54cccb5628867a17976517
Karl Yngve Lervåg <karl.yngve+git@gmail.com>
2025-08-27 16:10:31 +0200

    feat: better sql injection for scala

--- after/queries/scala/injections.scm
+++ after/queries/scala/injections.scm
@@ -2,7 +2,31 @@
 
 (interpolated_string_expression
   (identifier) @type
-    (#any-of? @type "SQL" "sql")
+  (#any-of? @type "SQL" "sql")
   (interpolated_string) @injection.content
-    (#offset! @injection.content 0 3 0 -3)
-  (#set! injection.language "sql"))
+  (#offset! @injection.content 0 3 0 -3)
+  (#set! injection.language "sql")
+)
+
+(
+  (call_expression
+     function: (field_expression
+                 field: (identifier) @methodName
+                 (#eq? @methodName "execute"))
+     arguments: (arguments
+                  (string) @injection.content))
+
+  (#set! injection.language "sql")
+  (#offset! @injection.content 0 1 0 -1))
+
+(
+  (call_expression
+     function: (field_expression
+                 field: (identifier) @methodName
+                 (#eq? @methodName "execute"))
+     arguments: (arguments
+                  (interpolated_string_expression
+                    (interpolated_string) @injection.content)))
+
+  (#set! injection.language "sql")
+  (#offset! @injection.content 0 3 0 -3))
