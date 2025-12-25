da6080b4875838122d917e784d3564c398f2c949
Karl Yngve Lervåg <karl.yngve+git@gmail.com>
2025-12-25 11:55:26 +0100

    feat: use builtin treesitter fold
    refer: https://github.com/neovim/neovim/issues/36739

--- ftplugin/json.lua
+++ ftplugin/json.lua
@@ -1,3 +1,3 @@
 vim.wo[0][0].foldmethod = "expr"
-vim.wo[0][0].foldexpr = "nvim_treesitter#foldexpr()"
+vim.wo[0][0].foldexpr = "v:lua.vim.treesitter.foldexpr()"
 vim.wo[0][0].foldlevel = 1
--- ftplugin/yaml.lua
+++ ftplugin/yaml.lua
@@ -1,2 +1,3 @@
-vim.wo[0][0].foldmethod = "indent"
+vim.wo[0][0].foldmethod = "expr"
+vim.wo[0][0].foldexpr = "v:lua.vim.treesitter.foldexpr()"
 vim.wo[0][0].foldlevel = 1
