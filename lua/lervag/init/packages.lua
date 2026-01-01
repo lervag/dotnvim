a549ce988318d18d419428ccd3fc14758bd9987c
Karl Yngve Lervåg <karl.yngve+git@gmail.com>
2026-01-01 16:11:12 +0100

    feat: remove unnecessary fix for matchup
    The fix is not necessary after:
    https://github.com/andymass/vim-matchup/pull/423

--- lua/lervag/init/packages.lua
+++ lua/lervag/init/packages.lua
@@ -259,8 +259,6 @@ local M = {
       vim.g.matchup_matchparen_deferred = 1
       vim.g.matchup_matchparen_offscreen = { method = "popup" }
       vim.g.matchup_override_vimtex = 1
-      -- https://github.com/andymass/vim-matchup/issues/416
-      vim.g.matchup_treesitter_disabled = { "markdown" }
     end,
   },
   {
