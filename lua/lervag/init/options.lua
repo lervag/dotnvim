6aaf693dfd6c7cba94131221928ad318cf158191
Karl Yngve Lervåg <karl.yngve+git@gmail.com>
2026-09-19 17:43:45 +0200

    feat: disable python3 provider

--- lua/lervag/init/options.lua
+++ lua/lervag/init/options.lua
@@ -159,7 +159,7 @@ vim.g.loaded_perl_provider = 0
 vim.g.loaded_python_provider = 0
 vim.g.loaded_ruby_provider = 0
 
-vim.g.python3_host_prog = "~/.local/venvs/nvim/bin/python"
+vim.g.python3_host_prog = 0
 
 vim.g.vimsyn_embed = "lP"
 vim.g.man_hardwrap = 1
